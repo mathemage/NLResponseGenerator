9210917b17e4e399ac97a1072eead57b2f90a4e7
Karel Ha <mathemage@gmail.com>
2017-06-17 16:08:55 +0200

    Create helloWorld.kt

---
+++ tree-parse/src/helloWorld.kt
@@ -0,0 +1,13 @@
+/**
+ * @author mathemage <ha@h2o.ai>
+ * created on 17.6.17
+ */
+
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
