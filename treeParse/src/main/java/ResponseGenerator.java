87cec3cdcf657c763ad3041544997bac514eed2a
Karel Ha <mathemage@gmail.com>
2017-06-18 06:56:32 +0200

    Import template for findNN from fastText

--- treeParse/src/main/java/ResponseGenerator.java
+++ treeParse/src/main/java/ResponseGenerator.java
@@ -24,6 +24,48 @@ public class ResponseGenerator {
 		return mockVector;
 	}
 	
+/*	based on https://github.com/facebookresearch/fastText/blob/master/src/fasttext.cc#L442
+
+	void FastText::findNN(const Matrix& wordVectors, const Vector& queryVec,
+	                      int32_t k, const std::set<std::string>& banSet) {
+		real queryNorm = queryVec.norm();
+		if (std::abs(queryNorm) < 1e-8) {
+			queryNorm = 1;
+		}
+		std::priority_queue<std::pair<real, std::string>> heap;
+		Vector vec(args_->dim);
+		for (int32_t i = 0; i < dict_->nwords(); i++) {
+			std::string word = dict_->getWord(i);
+			real dp = wordVectors.dotRow(queryVec, i);
+			heap.push(std::make_pair(dp / queryNorm, word));
+		}
+		int32_t i = 0;
+		while (i < k && heap.size() > 0) {
+			auto it = banSet.find(heap.top().second);
+			if (it == banSet.end()) {
+				std::cout << heap.top().second << " " << heap.top().first << std::endl;
+				i++;
+			}
+			heap.pop();
+		}
+	}
+	
+	void FastText::nn(int32_t k) {
+		std::string queryWord;
+		Vector queryVec(args_->dim);
+		Matrix wordVectors(dict_->nwords(), args_->dim);
+		precomputeWordVectors(wordVectors);
+		std::set<std::string> banSet;
+		std::cout << "Query word? ";
+		while (std::cin >> queryWord) {
+			banSet.clear();
+			banSet.insert(queryWord);
+			getVector(queryVec, queryWord);
+			findNN(wordVectors, queryVec, k, banSet);
+			std::cout << "Query word? ";
+		}
+	}*/
+	
 	public static void main(String[] args) {
 		generateResponses(mockStarter);
 	}
